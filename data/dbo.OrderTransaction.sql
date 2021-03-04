84316a898d53a786a6c3f068ef6fa5604d5a7b31
dharmik11-coder <60151664+dharmik11-coder@users.noreply.github.com>
2021-03-04 18:56:48 -0500

    Add files via upload

---
+++ data/dbo.OrderTransaction.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[OrderTransaction] (
+    [OrderTransaction_Id] INT             IDENTITY (1, 1) NOT NULL,
+    [Oder_Id]             INT             NULL,
+    [Item_Id]             INT             NULL,
+    [Quantity]            INT             NULL,
+    [Amount]              DECIMAL (10, 2) NULL,
+    [Total]               DECIMAL (10, 2) NULL,
+    PRIMARY KEY CLUSTERED ([OrderTransaction_Id] ASC), 
+    CONSTRAINT [FK_OrderTransaction_Order] FOREIGN KEY ([Order_Id]) REFERENCES [dbo].[Order] ([Order_Id]), 
+    CONSTRAINT [FK_OrderTransaction_Item] FOREIGN KEY ([Item_Id]) REFERENCES [dbo].[Item] ([Item_Id]),
+
+);
+
---
+++ data/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Supplier]
+(
+	[Supplier_Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Supplier_Name] NVARCHAR(50) NULL, 
+    [Address] NVARCHAR(50) NULL, 
+    [Pincode] INT NULL, 
+    [Email_Id] NVARCHAR(50) NULL, 
+    [Contact] NVARCHAR(50) NULL
+)
